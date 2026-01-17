c8fc0e598ee87ec4c0a28d5abdc9de77726222e2
suren <surenpal666@gmail.com>
2026-01-18 00:01:59 +0900

    first commit for the jan 18

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,13 @@
 export default function Navbar() {
   return (
-    <nav className="w-full p-4 bg-indigo-400 shadow-md fixed top-0 left-0 z-50">
+    <nav className="w-full h-16 p-4 bg-gray-200 shadow-md fixed top-0 left- z-50 text-xl font-semibold text-lavender-600">
       <div className="max-w-6xl mx-auto flex justify-between items-center">
-        <h1 className="text-xl font-bold">Suren Pal</h1>
+        <h1 className="text-xl font-bold text-lavender-600">Suren Pal</h1>
         <div className="space-x-6">
-          <a href="#about">About</a>
-          <a href="#skills">Skills</a>
-          <a href="#projects">Projects</a>
-          <a href="#contact">Contact</a>
+          <a  href="#about">About</a>
+          <a  href="#skills">Skills</a>
+          <a  href="#projects">Projects</a>
+          <a  href="#contact">Contact</a>
         </div>
       </div>
     </nav>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,14 +9,14 @@ export default function Project() {
       <ProjectCard 
         imgSrc={"./src/assets/weather-app.png"}
         title="Weather-App"
-        description="A clean portfolio built with React and Tailwind."
+        description="A clean weather app built with React and Tailwind."
         url={"https://weatherappsuren.vercel.app/"}
       />
 
       <ProjectCard 
         imgSrc={"./src/assets/date-time-app.png"}
         title="Date-Time-App"
-        description="A clean portfolio built with React and Tailwind."
+        description="A clean date-time app built with React and Tailwind."
         url={"https://datetimeappsuren.vercel.app"}
       />
 
