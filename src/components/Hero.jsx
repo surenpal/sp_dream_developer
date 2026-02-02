49bde20dfd8aa26f9c92bd8a2b2060df20a5c7e4
suren <surenpal666@gmail.com>
2026-02-02 18:20:31 +0900

    color for the button of the hero section is changed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 export default function Hero() {
   return (
     <section className="min-h-screen flex flex-col justify-center items-center text-center px-6 bg-gray-50">
-      <h1 className="text-5xl font-extrabold mb-4 text-gray-800">
+      <h1 className="text-5xl font-extrabold mb-4 text-green-600">
         Hi, I'm Suren Pal
       </h1>
 
@@ -11,7 +11,7 @@ export default function Hero() {
 
       <a
         href="#projects"
-        className="px-8 py-3 bg-blue-600 text-white rounded-lg shadow-lg hover:bg-blue-700 hover:shadow-xl transition-all duration-300"
+        className="px-8 py-3 bg-pink-400 text-white rounded-lg shadow-lg hover:bg-pink-600 hover:shadow-xl transition-all duration-300"
       >
         Upcoming Projects
       </a>
